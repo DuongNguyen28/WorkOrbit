6843de18bd586900e13b29d9d6c4bc1d9c5d6f3e
lekhoavu2909 <vulk29@gmail.com>
2025-04-28 20:17:45 -0400

    fix translate
    Signed-off-by: lekhoavu2909 <vulk29@gmail.com>

--- backend/controllers/translation_pdf_to_doc_controller.py
+++ backend/controllers/translation_pdf_to_doc_controller.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse
 from fastapi import APIRouter, Depends, Form, HTTPException, UploadFile, File
 from fastapi.responses import FileResponse
 from sqlalchemy.orm import Session
@@ -8,11 +9,22 @@ from ..services.pdf_to_pdf_service import PdfToPdfTranslationService
 from ..controllers.auth_controller import get_db
 from ..schemas.file import FileCreate
 from ..services.file_service import save_file_record
+from fastapi.responses import StreamingResponse
+from google.cloud import storage
+import io
+from dotenv import load_dotenv
+load_dotenv()
 
 
 router = APIRouter()
 pdf_to_docx_translator_service = PdfToDocxTranslatorService()
 pdf_to_pdf_translator_service = PdfToPdfTranslationService()
+
+def get_gcs_blob(blob_name: str) -> bytes:
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(os.getenv("GCS_BUCKET_NAME"))
+    blob = bucket.blob(blob_name)
+    return blob.download_as_bytes()
 
 @router.post("/translate/document")
 async def translate_pdf(
@@ -54,10 +66,28 @@ async def translate_pdf(
         source    = "translated",
     ))
 
-    os.unlink(pdf_path)
+    parsed_url = urlparse(result["trans_url"])
+    path = parsed_url.path.lstrip('/')
 
-    return FileResponse(
-        path        = local_out,
+    bucket_name = os.getenv("GCS_BUCKET_NAME")
+
+    # Remove bucket name prefix if it exists
+    if path.startswith(f"{bucket_name}/"):
+        blob_name = path[len(bucket_name) + 1:]  # +1 to remove the extra slash
+    else:
+        blob_name = path
+
+    blob_bytes = get_gcs_blob(blob_name)
+    file_like = io.BytesIO(blob_bytes)
+
+    # if os.path.exists(local_out):
+    #     os.unlink(local_out)
+    
+    # if os.path.exists(pdf_path):
+    #     os.unlink(pdf_path)
+
+    return StreamingResponse(
+        file_like,
         media_type  = media_type,
-        filename    = os.path.basename(local_out),
+        headers={"Content-Disposition": f"attachment; filename={os.path.basename(local_out)}"}
     )
